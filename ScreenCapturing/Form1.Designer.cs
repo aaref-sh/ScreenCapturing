44fdfad6fcf77ccfd362552862f57d38c290e04f
aaref-sh <m.aaref-sh@hotmail.com>
2021-04-04 22:07:49 +0300

    Add project files.

---
+++ ScreenCapturing/Form1.Designer.cs
@@ -0,0 +1,153 @@
+﻿
+namespace ScreenCapturing
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.l2 = new System.Windows.Forms.Label();
+            this.l1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.DarkGray;
+            this.panel1.Controls.Add(this.panel2);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.ForeColor = System.Drawing.Color.DarkGray;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(800, 450);
+            this.panel1.TabIndex = 2;
+            // 
+            // l2
+            // 
+            this.l2.AutoSize = true;
+            this.l2.BackColor = System.Drawing.Color.White;
+            this.l2.Location = new System.Drawing.Point(72, 13);
+            this.l2.Name = "l2";
+            this.l2.Size = new System.Drawing.Size(35, 13);
+            this.l2.TabIndex = 3;
+            this.l2.Text = "label1";
+            this.l2.MouseDown += new System.Windows.Forms.MouseEventHandler(this.l1_MouseDown);
+            this.l2.MouseEnter += new System.EventHandler(this.l2_MouseEnter);
+            this.l2.MouseLeave += new System.EventHandler(this.l2_MouseLeave);
+            this.l2.MouseMove += new System.Windows.Forms.MouseEventHandler(this.l2_MouseMove);
+            this.l2.MouseUp += new System.Windows.Forms.MouseEventHandler(this.l1_MouseUp);
+            // 
+            // l1
+            // 
+            this.l1.AutoSize = true;
+            this.l1.BackColor = System.Drawing.Color.White;
+            this.l1.Location = new System.Drawing.Point(71, 0);
+            this.l1.Name = "l1";
+            this.l1.Size = new System.Drawing.Size(35, 13);
+            this.l1.TabIndex = 2;
+            this.l1.Text = "label1";
+            this.l1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.l1_MouseDown);
+            this.l1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.l1_MouseMove);
+            this.l1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.l1_MouseUp);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(0, 3);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(0, 0);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.panel3);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Right;
+            this.panel2.Location = new System.Drawing.Point(690, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(110, 450);
+            this.panel2.TabIndex = 4;
+            // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.button2);
+            this.panel3.Controls.Add(this.l1);
+            this.panel3.Controls.Add(this.l2);
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel3.Location = new System.Drawing.Point(0, 423);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(110, 27);
+            this.panel3.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.TransparencyKey = System.Drawing.Color.DarkGray;
+            this.panel1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label l1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label l2;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel3;
+    }
+}
+
---
+++ ScreenCapturing/Form1.cs
@@ -0,0 +1,215 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Drawing.Imaging;
+using System.Diagnostics;
+using System.IO;
+using System.Security.Cryptography;
+using System.Threading;
+using System.Runtime.InteropServices;
+using ScreenCapturing.classes;
+using Microsoft.AspNetCore.SignalR.Client;
+
+namespace ScreenCapturing
+{
+    public partial class Form1 : Form
+    {
+        HubConnection connection;
+        Bitmap[,] sa = new Bitmap[2,3];
+        Bitmap[,] prev = new Bitmap[2,3];
+        int height, width;
+        public Form1()
+        {
+
+            InitializeComponent();
+            updatesize();
+            ConfigSignalRConnection();
+            encoderParameters.Param[0] = new EncoderParameter(Encoder.Quality, 100L);
+        }
+
+        private async void ConfigSignalRConnection()
+        {
+            connection = new HubConnectionBuilder()
+                .WithUrl("http://localhost:5000/CastHub")
+                .WithAutomaticReconnect()
+                .Build();
+            connection.On<string,int,int>("UpdateScreen", UpdateScreen);
+            await connection.StartAsync();
+            //MessageBox.Show( await connection.InvokeAsync<string>("ok"));
+        }
+
+        private void UpdateScreen(string base64,int r,int c)
+        {
+            Image.FromStream(new MemoryStream(Convert.FromBase64String(base64)))
+                .Save("D:\\sc\\sc"+r+" "+c+".jpg");
+        }
+        private readonly Stopwatch sw = new Stopwatch();
+        private Bitmap CaptureImage(int x, int y, int i, int j)
+        {
+            Bitmap b = new Bitmap(width, height);
+            using (Graphics g = Graphics.FromImage(b))
+                g.CopyFromScreen(x + width * j, y + height * i, 0, 0, b.Size, CopyPixelOperation.SourceCopy);
+
+            return b;
+        }
+        EncoderParameters encoderParameters = new EncoderParameters(1);
+
+        public void Cap()
+        {
+            try
+            {
+                for (int i = 0; i < 2; i++)
+                    for (int j = 0; j < 3; j++)
+                    {
+                        //   sa[i, j] = GetDesktopImage(i,j);
+                        sa[i, j] = CaptureImage(this.Left, this.Top, i, j);
+                        if (!Compare(prev[i, j], sa[i, j]))
+                        {
+                            try
+                            {
+                                using (MemoryStream ms = new MemoryStream())
+                                {
+                                    sa[i, j].Save(ms, ImageFormat.Jpeg);
+                                    string base64 = Convert.ToBase64String(ms.ToArray());
+                                    connection.InvokeAsync("UpdateScreen", base64, i, j);
+                                }
+                            }
+                            catch (Exception e) { MessageBox.Show(e.Message); }
+                            prev[i, j] = sa[i, j];
+                        }
+                    }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+
+        }
+        Thread caster = null;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var d = DateTime.Now;
+            sw.Start();
+            caster = new Thread(cast);
+            caster.Start();
+            sw.Stop();
+            MessageBox.Show((DateTime.Now - d).ToString());
+        }
+        void cast()
+        {
+            while(connection.State == HubConnectionState.Connected)
+                Cap();
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            caster.Abort();
+        }
+        private static ImageCodecInfo GetEncoder(ImageFormat format)
+        {
+            var codecs = ImageCodecInfo.GetImageDecoders();
+            foreach (var codec in codecs)
+                if (codec.FormatID == format.Guid)
+                    return codec;
+            return null;
+        }
+        public static bool Compare(Bitmap bmp1, Bitmap bmp2)
+        {
+            if (bmp1 == null || bmp2 == null)
+                return false;
+            if (object.Equals(bmp1, bmp2))
+                return true;
+            if (!bmp1.Size.Equals(bmp2.Size) || !bmp1.PixelFormat.Equals(bmp2.PixelFormat))
+                return false;
+
+            int bytes = bmp1.Width * bmp1.Height * (Image.GetPixelFormatSize(bmp1.PixelFormat) / 8);
+
+            bool result = true;
+            byte[] b1bytes = new byte[bytes];
+            byte[] b2bytes = new byte[bytes];
+
+            BitmapData bitmapData1 = bmp1.LockBits(new Rectangle(0, 0, bmp1.Width, bmp1.Height), ImageLockMode.ReadOnly, bmp1.PixelFormat);
+            BitmapData bitmapData2 = bmp2.LockBits(new Rectangle(0, 0, bmp2.Width, bmp2.Height), ImageLockMode.ReadOnly, bmp2.PixelFormat);
+
+            Marshal.Copy(bitmapData1.Scan0, b1bytes, 0, bytes);
+            Marshal.Copy(bitmapData2.Scan0, b2bytes, 0, bytes);
+
+            for (int n = 0; n <= bytes - 1; n++)
+            {
+                if (b1bytes[n] != b2bytes[n])
+                {
+                    result = false;
+                    break;
+                }
+            }
+
+            bmp1.UnlockBits(bitmapData1);
+            bmp2.UnlockBits(bitmapData2);
+
+            return result;
+        }
+        #region resize and drag
+        private void l1_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                drag = true;
+                posX = Cursor.Position.X - this.Left;
+                posY = Cursor.Position.Y - this.Top;
+            }
+        }
+        private void l1_MouseUp(object sender, MouseEventArgs e)
+        {
+            drag = false;
+            updatesize();
+        }
+        private void l1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (drag)
+            {
+                this.Top = Cursor.Position.Y - posY;
+                this.Left = Cursor.Position.X - posX;
+            }
+            this.Cursor = Cursors.Default;
+        }
+        void updatesize()
+        {
+            height = this.Height / 2;
+            width = this.Width / 3;
+        }
+        int posX;
+        int posY;
+        bool drag;
+        private void l2_MouseEnter(object sender, EventArgs e)
+        {
+            this.Cursor = Cursors.SizeNWSE;
+        }
+        private void l2_MouseLeave(object sender, EventArgs e)
+        {
+            this.Cursor = Cursors.Default;
+        }
+        private void l2_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (drag)
+            {
+                Width = Cursor.Position.X - this.Left;
+                Height = Cursor.Position.Y - this.Top;
+            }
+        }
+        #endregion
+   
+        public Bitmap GetDesktopImage(int i,int j) {
+            IntPtr hDC = WIN32_API.GetDC(WIN32_API.GetDesktopWindow()); 
+            IntPtr hMemDC = WIN32_API.CreateCompatibleDC(hDC); 
+            m_HBitmap = WIN32_API.CreateCompatibleBitmap(hDC, width, height); 
+            if (m_HBitmap != IntPtr.Zero) { 
+                IntPtr hOld = (IntPtr)WIN32_API.SelectObject(hMemDC, m_HBitmap);
+                WIN32_API.BitBlt(hMemDC, 0, 0, width, height, hDC, this.Left+width*j, this.Top+height*i, WIN32_API.SRCCOPY); 
+                WIN32_API.SelectObject(hMemDC, hOld); WIN32_API.DeleteDC(hMemDC); 
+                WIN32_API.ReleaseDC(WIN32_API.GetDesktopWindow(), hDC); 
+                return Image.FromHbitmap(m_HBitmap); 
+            } 
+            return null; 
+        } 
+        protected IntPtr m_HBitmap; 
+    }
+}
